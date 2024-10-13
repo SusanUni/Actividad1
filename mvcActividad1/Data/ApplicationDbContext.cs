6e92a1709fdf4019ff33cb4cecd04e60f644e904
Susan Daniela Clemente Quispe <susandclementeq@gmail.com>
2024-10-12 23:03:51 -0400

    Primer commit

---
+++ mvcActividad1/Data/ApplicationDbContext.cs
@@ -0,0 +1,17 @@
+﻿using mvcActividad1.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace mvcActividad1.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+          : base(options)
+        {
+        }
+        //Aqui todos los modelos que se creen 
+        public DbSet<Productos> Productos { get; set; }
+    }
+
+
+}
---
+++ mvcActividad1/Models/Productos.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace mvcActividad1.Models
+{
+    public class Productos
+    {
+        [Key]
+        public int Id { get; set; }
+        [Required(ErrorMessage = "El nombre es obligatorio")]
+        [Display(Name = "Nombre del Productos")]
+        public string Nombre { get; set; }
+        [Required(ErrorMessage = "La descripción es obligatoria")]
+        public string Descripcion { get; set; }
+    }
+
+}
